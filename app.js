dca3f8d50b3a60b38fb05ee3553337f2928c4bd6
saramesa <saramesaguerra@gmail.com>
2020-04-19 14:27:57 +0200

    add under construction template

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Site under construction! (from Git)'))
+app.use(express.static("public"));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
