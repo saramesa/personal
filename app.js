bbf24d01d7389ecf7a204b5c17104bd0ebcf029d
Sara Mesa <saramesaguerra@gmail.com>
2020-04-22 14:56:21 +0200

    change app.js

--- app.js
+++ app.js
@@ -3,8 +3,4 @@ const app = express()
 const port = 3000
 
 app.use(express.static("public"));
-app.get('/chatbot', function (req, res) {
-    console.log('Request Type:', req.method);
-    res.send('se ejecuta la rest de chatbot')
-  });
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
