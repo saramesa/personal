a3256c3f8fc85eb3f0a649f88c072c3cb48a01d1
Sara Mesa <saramesaguerra@gmail.com>
2020-04-22 14:51:25 +0200

    add route

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ const app = express()
 const port = 3000
 
 app.use(express.static("public"));
-
+app.use('/chatbot', function (req, res) {
+    console.log('Request Type:', req.method);
+    res.send('se ejecuta la rest de chatbot')
+  });
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
